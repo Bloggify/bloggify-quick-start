04435cf6238fcd05f5bbe6f8abec36d4fcff15ff
Ionică Bizău <bizauionica@gmail.com>
2016-03-11 16:24:22 +0200

    Add page

--- .bloggify/plugins/a/index.js
+++ .bloggify/plugins/a/index.js
@@ -1,4 +1,21 @@
 module.exports = (conf, bloggify, ready) => {
+    bloggify.server.page.add("/", lien => {
+        lien.res.write("hi");
+        setTimeout(function () {
+            lien.res.write("hi");
+            setTimeout(function () {
+                lien.res.write("hi");
+                setTimeout(function () {
+                    lien.res.end("hi");
+
+                }, 1000);
+
+            }, 1000);
+        }, 1000);
+        //lien.end({
+        //    hello: "world"
+        //})
+    });
     console.log("Loaded plugin a", conf);
     setTimeout(() => {
         ready();
