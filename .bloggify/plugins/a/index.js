81cae9900753330e512f94fd285c96004a03a042
Ionică Bizău <bizauionica@gmail.com>
2016-03-22 12:04:54 +0200

    :memo:

--- .bloggify/plugins/a/index.js
+++ .bloggify/plugins/a/index.js
@@ -1,23 +1,11 @@
 module.exports = (conf, bloggify, ready) => {
-    bloggify.server.page.add("/", lien => {
-        lien.res.write("hi");
-        setTimeout(function () {
-            lien.res.write("hi");
-            setTimeout(function () {
-                lien.res.write("hi");
-                setTimeout(function () {
-                    lien.res.end("hi");
-
-                }, 1000);
-
-            }, 1000);
-        }, 1000);
-        //lien.end({
-        //    hello: "world"
-        //})
+    bloggify.addPage("/", lien => {
+        lien.end({
+            hello: "world"
+        })
     });
     console.log("Loaded plugin a", conf);
     setTimeout(() => {
         ready();
-    });
+    }, 10);
 };
