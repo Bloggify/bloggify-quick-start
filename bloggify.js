2b34b11010dfcdf94687e088073aa67d6fdae161
Ionică Bizău <bizauionica@gmail.com>
2017-09-13 17:15:46 +0300

    Add emoji and highlighting

--- bloggify.js
+++ bloggify.js
@@ -4,7 +4,10 @@ module.exports = {
     title: "Bloggify Starter"
   , description: "Welcome to Bloggify Starter!"
   , domain: "https://example.com"
-
+  , plugins: [
+      "emoji"
+    , ["markdown-highlight", { theme: "ascetic" }]
+    ]
     // This will enable the Articles/Pages APIs
   , adapter: ["bloggify-markdown-adapter", {
         theme: ["theme-light", {
